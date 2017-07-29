5517c463a9b367d67c8f1acfc5fbefd1a449bf48
Tris Setiawan <tris.setiawan@yahoo.com>
2017-07-29 04:30:04 +0000

    handle disconnect

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+client.on('disconnect', function(msg, code) {
+  if (code === 0) return console.error(msg);
+  client.login(process.env.token);
+});
+
 client.on('message', message => {
   if (message.author.bot)
     return;
